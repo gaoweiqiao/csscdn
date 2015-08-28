65350a2110969db187229616e344139dc8862095
patrick <gaoweiqiao@qq.com>
2015-08-28 13:58:52 +0800

    t

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Created by patrick on 15/8/28.
  */
-var div = document.getElementsByTagName("div");
+var div = document.getElementById("btn");
 div.onclick = function(){
     alert("hello github");
 };
